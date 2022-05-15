a11d75b30ea82bbd8cd51ecceec40b12ae36a05f
BeWoler <bewoler.del@gmail.com>
2022-05-15 23:40:56 +0400

    set up project

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PSQL_HOST,
+      port: Number(process.env.PSQL_PORT),
+      username: process.env.PSQL_USERNAME,
+      password: process.env.PSQL_PASSWORD,
+      database: process.env.PSQL_DB,
+      autoLoadModels: true,
+      models: [],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
